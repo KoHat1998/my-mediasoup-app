43683d933869bce6a9df38292d755cdd86fff6e2
Ko Hat <htetaungthant1998@gmail.com>
2025-08-07 16:50:01 +0700

    Initial mediasoup project

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const http = require('http');
+const app = express();
+const server = http.createServer(app);
+
+app.get('/', (req, res) => {
+    res.send('Hello mediasoup!');
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log('Server running on port', PORT);
+});
